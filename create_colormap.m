ee396b14cdb997ee75af372abb0d91f540e59807
spiggen <jonasvilgotlotberg@hotmail.se>
2023-10-07 14:40:44 +0200

    fixed bug with create_colormap where it duplicated the last color multiple times

--- create_colormap.m
+++ create_colormap.m
@@ -11,7 +11,7 @@ colormap_length = varargin{end};
 map = ones(colormap_length,1)*colors(end,:);
 indexes_per_color = floor(colormap_length/(number_of_colors-1));
 start_index = 1;
-end_index = start_index + indexes_per_color-1;
+end_index = start_index + indexes_per_color;
 for color_index = 1:number_of_colors-1
 
 map(start_index:end_index,1) = linspace(colors(color_index,1), colors(color_index+1,1), end_index-start_index +1)';
@@ -19,7 +19,7 @@ map(start_index:end_index,2) = linspace(colors(color_index,2), colors(color_inde
 map(start_index:end_index,3) = linspace(colors(color_index,3), colors(color_index+1,3), end_index-start_index +1)';
 
 start_index = end_index;
-end_index = start_index + indexes_per_color -1;
+end_index = start_index + indexes_per_color;
 end
 
 
